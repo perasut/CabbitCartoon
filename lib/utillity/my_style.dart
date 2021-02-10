26b23d533b25f20fc30a3401bde13612b8c897f8
sudo <perasutlife@gmail.com>
2021-02-10 19:30:39 +0700

    ch7

--- lib/utillity/my_style.dart
+++ lib/utillity/my_style.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MyStyle {
-  Color darkColor = Color(0x000081);
-  Color primaryColor = Color(0x4825b2);
-  Color lightColor = Color(0x7e52e5);
+  Color darkColor = Color(0xff000081);
+  Color primaryColor = Color(0xff4825b2);
+  Color lightColor = Color(0xff7e52e5);
 
-  Widget ShowLogo() => Image.asset('images/logo.png');
-  Widget TitleH1(String string) => Text(
+  Widget showLogo() => Image.asset('images/logo.png');
+  Widget titleH1(String string) => Text(
         string,
         style: TextStyle(
           fontSize: 30,
           fontWeight: FontWeight.bold,
-          // color: darkColor,
+          color: darkColor,
+        ),
+      );
+
+  Widget titleH2(String string) => Text(
+        string,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          color: darkColor,
+        ),
+      );
+
+  Widget titleH3(String string) => Text(
+        string,
+        style: TextStyle(
+          fontSize: 16,
+          // fontWeight: FontWeight.bold,
+          color: darkColor,
         ),
       );
   MyStyle();
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -10,20 +10,82 @@ class Authen extends StatefulWidget {
 
 class _AuthenState extends State<Authen> {
   double screen;
+  bool statusRedEye = true;
   @override
   Widget build(BuildContext context) {
     screen = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Titless'),
+        title: Text('Titlesssss'),
       ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             buildLogo(),
-            MyStyle().TitleH1('Cabbit Cartoon'),
+            MyStyle().titleH1('Cabbit Cartoon'),
+            buildUser(),
+            buildPassword(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Container buildUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: screen * 0.75,
+      child: TextField(
+        style: TextStyle(color: MyStyle().darkColor),
+        decoration: InputDecoration(
+          hintStyle: TextStyle(color: MyStyle().darkColor),
+          hintText: 'User:',
+          prefixIcon: Icon(
+            Icons.perm_identity,
+            color: MyStyle().darkColor,
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: MyStyle().darkColor)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),   
+              borderSide: BorderSide(color: MyStyle().lightColor)),
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: screen * 0.75,
+      child: TextField(
+        obscureText: statusRedEye,
+        style: TextStyle(color: MyStyle().darkColor),
+        decoration: InputDecoration(
+          suffixIcon: IconButton(
+              icon: statusRedEye
+                  ? Icon(Icons.remove_red_eye)
+                  : Icon(Icons.remove_circle),
+              onPressed: () {
+                setState(() {
+                  statusRedEye = !statusRedEye;
+                });
+                print('statusredeye = $statusRedEye');
+              }),
+          hintStyle: TextStyle(color: MyStyle().darkColor),
+          hintText: 'Password:',
+          prefixIcon: Icon(
+            Icons.lock_outline,
+            color: MyStyle().darkColor,
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: MyStyle().darkColor)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: MyStyle().lightColor)),
         ),
       ),
     );
@@ -32,7 +94,7 @@ class _AuthenState extends State<Authen> {
   Container buildLogo() {
     return Container(
       width: screen * 0.4,
-      child: MyStyle().ShowLogo(),
+      child: MyStyle().showLogo(),
     );
   }
 }
